5025921c65f50260662a544945fa4234b3f56953
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-21 12:47:34 -0600

    pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 const Contact = () => {
+  const router = useRouter();
+  const handleClick = () => router.push('/');
   return (
     <div>
       <h1>Contact</h1>
+      <Link href="/">Go Home</Link>
+      <Link href={`/blog`}>Go to Blog Post</Link>
+      <button type="button" onClick={handleClick}>
+        Home
+      </button>
     </div>
   );
 };
